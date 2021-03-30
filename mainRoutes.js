cb9f76603b4956e7bf9f0cc750624d40b738cb19
Nathan Jones <natmax.jones@gmail.com>
2021-03-30 22:32:40 +0200

    Made changes to be hosted on Azure

--- mainRoutes.js
+++ mainRoutes.js
@@ -7,7 +7,7 @@ const mainRouter = express.Router()
 // Routes
 // Defines a route for the GET request to the path '/'
 mainRouter.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World, I\'m Node.js')
 })
 
 mainRouter.get('/about', function (req, res) {
