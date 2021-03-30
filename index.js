ecec4af3ffd29092afacae8dbd4cb4f5b60d02eb
Nathan Jones <natmax.jones@gmail.com>
2021-03-30 21:46:33 +0200

    Added main router

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 'using strict'
 
-const path = require('path')
 const express = require('express')
 const app = express()
+const mainRouter = require('./mainRoutes')
 
-// Routes
-
-// Defines a route for the GET request to the path '/'
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+app.use(mainRouter)
 
 app.listen(3000)
 console.log('Express server running on port 3000')
---
+++ mainRoutes.js
@@ -0,0 +1,17 @@
+'using strict'
+
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+
+// Routes
+// Defines a route for the GET request to the path '/'
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+module.exports = mainRouter
